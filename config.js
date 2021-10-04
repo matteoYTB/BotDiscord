8105919e69354161d41aa2868ad7633087d0235e
matteoYTB <58906694+matteoYTB@users.noreply.github.com>
2021-10-04 19:39:45 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    app: {
+        px: '!',
+        token: 'oJNzgKt5cegtqPMCKVMIqZKMHRyzbolC',
+        playing: 'by Zerio ❤️'
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: 'DJ',
+            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'seek', 'shuffle', 'skip', 'stop', 'volume']
+        },
+        maxVol: 100,
+        loopMessage: false,
+        discordPlayer: {}
+    }
+};
